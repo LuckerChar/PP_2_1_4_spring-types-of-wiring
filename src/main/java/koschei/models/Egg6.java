4ac09bcc6ad345010fc92446765c7a7668fab1af
LuckerChar <kokakolka85@gmail.com>
2022-07-07 17:24:55 +0300

    new

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    private final Needle7 destroyNeedle;
+
+    public Egg6(Needle7 destroyNeedle) {
+        this.destroyNeedle = destroyNeedle;
+    }
+
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + destroyNeedle.toString();
     }
 }
